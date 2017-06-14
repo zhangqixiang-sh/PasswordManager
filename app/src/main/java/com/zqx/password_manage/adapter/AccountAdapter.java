87edc4939b2f2e8f7736981a0eb51545a6ffdfe2
zqx <zqx@zqx.com>
2017-06-14 20:20:25 +0800

    修复复制密码错误

--- app/src/main/java/com/zqx/password_manage/adapter/AccountAdapter.java
+++ app/src/main/java/com/zqx/password_manage/adapter/AccountAdapter.java
@@ -1,5 +1,7 @@
 package com.zqx.password_manage.adapter;
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +12,13 @@ import com.yanzhenjie.recyclerview.swipe.SwipeMenuAdapter;
 import com.zqx.password_manage.R;
 import com.zqx.password_manage.global.GlobalData;
 import com.zqx.password_manage.model.bean.AccountBean;
+import com.zqx.password_manage.util.ToastUtil;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by ZhangQixiang on 2017/2/13.
@@ -53,17 +57,30 @@ public class AccountAdapter extends SwipeMenuAdapter<AccountAdapter.ViewHolder> 
         return mDatas.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+     class ViewHolder extends RecyclerView.ViewHolder {
+        private final Context mContext;
         @BindView(R.id.tv_server)
         TextView mTvServer;
         @BindView(R.id.tv_name)
         TextView mTvName;
         @BindView(R.id.tv_pwd)
         TextView mTvPwd;
+        private ClipboardManager mCm;
+
 
         ViewHolder(View view) {
             super(view);
+            mContext = view.getContext();
             ButterKnife.bind(this, view);
+            mCm = ((ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE));
+
+        }
+
+        @OnClick(R.id.iv_copy_pwd)
+        public void onCopyClick(View view) {
+            AccountBean bean = mDatas.get(getAdapterPosition());
+            mCm.setText(bean.pwd);
+            ToastUtil.show("已复制密码到剪贴板");
         }
 
     }
